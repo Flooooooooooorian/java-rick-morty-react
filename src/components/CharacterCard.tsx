eb474e1f971878d03f2bfc45fbbb429b31b11585
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2022-10-20 17:29:40 +0200

    props

--- src/components/CharacterCard.tsx
+++ src/components/CharacterCard.tsx
@@ -5,11 +5,11 @@ type CharacterCardProps = {
     character : Character
 }
 
-export default function CharacterCard( { character } : CharacterCardProps ){
+export default function CharacterCard( props : CharacterCardProps ){
 
     return <div className="character-card">
-        <h3 className="character-card__name">{character.name}</h3>
-        <img className="character-card__image" src={character.image} alt={character.name}/>
-        <div className="character-card__origin">{character.origin.name}</div>
+        <h3 className="character-card__name">{props.character.name}</h3>
+        <img className="character-card__image" src={props.character.image} alt={props.character.name}/>
+        <div className="character-card__origin">{props.character.origin.name}</div>
     </div>
 }
